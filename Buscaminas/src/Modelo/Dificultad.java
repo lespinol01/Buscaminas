ca494f5a858e3076a8f3502b122ae6e16d15921c
Usuario <Usuario@LAPTOP-NVBAHJE3.home>
2020-03-22 18:46:51 +0100

    Versión inicial

---
+++ Buscaminas/src/Modelo/Dificultad.java
@@ -0,0 +1,21 @@
+package Modelo;
+
+public class Dificultad {
+	private int numeroMinas, tamanoTablero;
+
+	public Dificultad(int numeroMinas, int tamanoTablero) {
+		super();
+		this.numeroMinas = numeroMinas;
+		this.tamanoTablero = tamanoTablero;
+	}
+
+	public int getNumeroMinas() {
+		return numeroMinas;
+	}
+
+	public int getTamanoTablero() {
+		return tamanoTablero;
+	}
+	
+	
+}
